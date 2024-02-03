4fb6ac6ee76449ab69509492e13a7f64e8c88527
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-03 10:17:38 +0100

    added login and register component

--- front-end/plateforme/src/app/login/login.component.ts
+++ front-end/plateforme/src/app/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Здесь обработайте вход пользователя, например, отправьте данные на сервер
+      console.log(this.loginForm.value);
+    }
+  }
 }
+
