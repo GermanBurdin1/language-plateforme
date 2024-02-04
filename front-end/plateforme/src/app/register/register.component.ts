7eea6d8d892956e0e667083c981c9465a0ecac9e
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-04 17:43:51 +0100

    реализовать автоматическую проверку

--- front-end/plateforme/src/app/register/register.component.ts
+++ front-end/plateforme/src/app/register/register.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
-// Определение интерфейса для данных формы
 interface FormData {
   e_mail?: string;
   login?: string;
@@ -15,12 +16,11 @@ interface FormData {
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   loginExists = false;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
-    // Инициализация формы с полями
     this.registerForm = this.fb.group({
       emailOrLogin: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -28,48 +28,47 @@ export class RegisterComponent {
     });
   }
 
-  checkLoginExistence(login: string): void {
-    this.http.get<{ exists: boolean }>(`http://learn-lang-platform.local/back-end/AngularRequestsHandler.php?login=${login}`).subscribe({
-      next: (response) => {
-        this.loginExists = response.exists;
-        if(this.loginExists) {
+  ngOnInit() {
+    this.registerForm.get('emailOrLogin')?.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(value => this.checkLoginExistence(value))
+    ).subscribe(exists => this.loginExists = exists);
+  }
+
+  checkLoginExistence(login: string): Observable<boolean> {
+    return this.http.get<{ exists: boolean }>(`http://learn-lang-platform.local/back-end/AngularRequestsHandler.php?login=${login}`).pipe(
+      switchMap(response => {
+        if (response.exists) {
           this.registerForm.controls['emailOrLogin'].setErrors({ 'loginExists': true });
         }
-      },
-      error: (error) => {
-        console.error('Ошибка при проверке логина:', error);
-      }
-    });
+        return [response.exists];
+      })
+    );
   }
 
   onSubmit() {
-
     if (this.registerForm.valid) {
-      // Создание объекта formData с использованием интерфейса
       let formData: FormData = {
         password: this.registerForm.value.password,
         name: this.registerForm.value.name
       };
 
-      // Определение, является ли введенное значение e-mail или логином
       const emailOrLogin = this.registerForm.value.emailOrLogin;
       if (emailOrLogin.includes('@')) {
-        // Если значение является e-mail
         formData.e_mail = emailOrLogin;
       } else {
-        // Если значение является логином
         formData.login = emailOrLogin;
       }
 
       const headers = new HttpHeaders().set('Content-Type', 'application/json');
-      // Отправка данных на сервер
       this.http.post('http://learn-lang-platform.local/back-end/AngularRequestsHandler.php', formData, { headers }).subscribe({
         next: (response) => console.log('Успех:', response),
         error: (error) => console.error('Ошибка:', error)
       });
-    }
-    else {
+    } else {
       console.error('Форма невалидна:', this.registerForm.errors);
     }
   }
 }
+
