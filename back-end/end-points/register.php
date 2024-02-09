b1c7ca0c0026d578cda205c816be6a098c189d8c
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-09 09:24:14 +0100

    22.2

---
+++ back-end/end-points/register.php
@@ -0,0 +1,44 @@
+
+<!-- register.php выступает посредником -->
+
+<?php
+require 'vendor/autoload.php'; // Подключаем библиотеку для отправки HTTP запросов
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $inputData = json_decode(file_get_contents('php://input'), true);
+    $email = $inputData['email'];
+    $login = $inputData['login'];
+    $password = $inputData['password'];
+
+    // Создаем HTTP клиент
+    $client = new Client(['base_uri' => 'http://learn-lang-platform.local']);
+
+    // Отправляем запрос на сервер для регистрации нового пользователя
+    try {
+        $response = $client->post('AngularRequestsHandler.php', [
+            'json' => [
+                'action' => 'register',
+                'email' => $email,
+                'login' => $login,
+                'password' => $password
+            ]
+        ]);
+
+        $responseData = json_decode($response->getBody(), true);
+
+        // Выводим результат регистрации
+        echo json_encode($responseData);
+    } catch (RequestException $e) {
+        // Обработка сетевых ошибок
+        $responseBody = $e->getResponse() ? $e->getResponse()->getBody(true) : null;
+        $errorMessage = $responseBody ? json_decode($responseBody, true) : ['error' => 'An error occurred during registration.'];
+        echo json_encode($errorMessage);
+    } catch (Exception $e) {
+        // Обработка прочих ошибок
+        echo json_encode(['error' => 'An unexpected error occurred.']);
+    }
+}
+?>
+
--- back-end/vendor/composer/autoload_real.php
+++ back-end/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitcc09c5ef818b186384836f14c845e711
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitcc09c5ef818b186384836f14c845e711::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
