a4bef19b3b1f2569084dfa94f341126491398b59
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-21 14:54:14 +0100

    2/ 1. визуальная составляющая чата

--- front-end/plateforme/src/app/dashboard/student/features/myPage/chat/chat.component.ts
+++ front-end/plateforme/src/app/dashboard/student/features/myPage/chat/chat.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+  messages: any[] = [];
+  newMessage: string = '';
 
+  constructor() { }
+
+  sendMessage() {
+    if (this.newMessage.trim() !== '') {
+      this.messages.push({
+        user: 'Пользователь', // Замените на динамическое значение, если необходимо
+        content: this.newMessage
+      });
+      this.newMessage = '';
+    }
+  }
 }
