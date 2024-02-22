27e1c1d414d472760af51e768219d8967959d46a
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-02-22 21:03:58 +0100

    created service for dictionary

---
+++ front-end/plateforme/src/app/dashboard/student/features/dictionary/dictionary.service.ts
@@ -0,0 +1,27 @@
+// src/app/services/word.service.ts
+
+import { Injectable } from '@angular/core';
+import { Word } from '../models/word.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WordService {
+  private words: Word[] = [];
+  private idCounter = 0;
+
+  constructor() {}
+
+  addWord(original: string, translation: string): void {
+    const word: Word = {
+      id: this.idCounter++,
+      original,
+      translation,
+    };
+    this.words.push(word);
+  }
+
+  getWords(): Word[] {
+    return this.words;
+  }
+}
