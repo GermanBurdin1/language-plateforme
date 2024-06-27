d6e62394a1b0d1fc1d905f220ed7b867e3248b2f
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-06-27 09:13:13 +0200

    нижний регистр для роли и убрал лишние символы roleResponse.role.trim().toLowerCase()

--- front-end/plateforme/src/app/auth.service.ts
+++ front-end/plateforme/src/app/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -13,19 +12,21 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login.php`, {e_mail: email, password })
-    .pipe(
-      tap(response => {
-        console.log('Ответ от сервера на логин:', response)
-        // Предположим, что ответ содержит поля token и role
-        if (response.token && response.role) {
-          localStorage.setItem('token', response.token);
-          localStorage.setItem('role', response.role);
-          localStorage.setItem('Id_person', response.Id_person);
-        }
-      }),
-      catchError(this.handleError)
-    );
+    return this.http.post<any>(`${this.apiUrl}/login.php`, { e_mail: email, password })
+      .pipe(
+        tap(response => {
+          console.log('Ответ от сервера на логин:', response);
+          if (response.token && response.role) {
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('role', response.role);
+            localStorage.setItem('Id_person', response.Id_person);
+            console.log('Auth.service:Токен и роль сохранены в localStorage');
+          } else {
+            console.error('Не удалось получить токен или роль из ответа');
+          }
+        }),
+        catchError(this.handleError)
+      );
   }
 
   register(userDetails: any): Observable<any> {
@@ -40,16 +41,14 @@ export class AuthService {
       );
   }
 
-
   getDashboardData(): Observable<any> {
-    // Тут предполагается, что токен уже сохранен в сервисе или localStorage
     const token = this.getToken();
     return this.http.get<any>(`${this.apiUrl}/dashboard.php`, {
       headers: { Authorization: `Bearer ${token}` }
     })
     .pipe(
       catchError(this.handleError)
-    );;
+    );
   }
 
   getUserName(token: string): Observable<any> {
@@ -64,7 +63,6 @@ export class AuthService {
     );
   }
 
-
   getUserRole(token: string): Observable<any> {
     const getUserUrl = 'http://learn-lang-platform.local/back-end/end-points/user-details/getUserRole.php';
     return this.http.get<any>(getUserUrl, {
@@ -76,37 +74,16 @@ export class AuthService {
     );
   }
 
-  // getUserRole(): Observable<string | null> {
-  //   const token = this.getToken();
-  //   if (!token) {
-  //     // Можно использовать of(null) из RxJS для создания Observable, который сразу завершится с null,
-  //     // если это приемлемо для вашей логики обработки отсутствия токена.
-  //     // Это позволит сохранить ваш интерфейс возвращаемого типа Observable<string | null>.
-  //     return of(null);
-  //   }
-  //   const role = localStorage.getItem('userRole');
-  //   return of(role);
-  // }
-
-  saveUserRole(role: string): void {
-  }
-
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // Случилась ошибка на стороне клиента или сети
       console.error('An error occurred:', error.error.message);
     } else {
-      // Сервер вернул код состояния ошибки
-      console.error(`Server returned code ${error.status}, ` +
-                    `body was: ${error.error}`);
+      console.error(`Server returned code ${error.status}, body was: ${error.error}`);
     }
-    // Вернуть Observable с пользовательским сообщением об ошибке
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 
-  // Добавьте метод для получения сохраненного токена
   getToken(): string | null {
-    // Получение токена из localStorage или вашего сервиса
     return localStorage.getItem('token');
   }
 
@@ -114,4 +91,3 @@ export class AuthService {
     return localStorage.getItem('Id_person');
   }
 }
-
